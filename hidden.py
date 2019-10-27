00bb5e9f2616bc46a1e069248d1353a0a42c6e18
kubatska <47230894+kubatska@users.noreply.github.com>
2019-10-27 15:57:22 +0200

    Update hidden.py

--- hidden.py
+++ hidden.py
@@ -4,15 +4,8 @@
 # Create new App and get the four strings
 
 
-# def oauth():
-#     return {"consumer_key": "0PlKlKMbNYWL9UeVuC6DPxX4B",
-#             "consumer_secret": "dpvpCGjA9udpjsPkCVfdxZhmAOLMXFeZcZEVLbH3usRiv8fHfN",
-#             "token_key": "963011359737774080-mYiiiIGOarKbuYGVdVTJBPcYC39RkqC",
-#             "token_secret": "f8031GVQo6dJXX35RRVbDlGxMcitrAt7uyAUVgKuO1E0R"}
-
-
 def oauth():
-    return {"consumer_key": "bS1YR8qtrcbZF95SvJ8T6sLpR",
-            "consumer_secret": "sNnOcgRltzkiXtyM5JBGdoUotwH2Et6IAk1rABibjlj2zqq1om",
-            "token_key": "963749242685870080-nVNcahW31mBaOVQIoseW6YT3H0ih5gA",
-            "token_secret": "CkAB7cpfZFtwdZTWyN4Eiu8FZkV6zvMqYkDoI7TL4jEvS"}
+    return {"consumer_key": "///",
+            "consumer_secret": "///",
+            "token_key": "///",
+            "token_secret": "///"}
